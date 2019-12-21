3a1567758172b2d4feddcbc7ee81bea88f31b693
Antonio Delgado <aa.antonio.delgado@gmail.com>
2019-12-22 00:09:49 +0100

    Modificaciones para IOS

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Imports 
 importScripts('js/sw-utils.js');
 
-const CACHE_STATIC = 'static-v2';
+const CACHE_STATIC = 'static-v3';
 const CACHE_DYNAMIC = 'dynamic-v1';
 const CACHE_INMUTABLE = 'inmutable-v1';
 
